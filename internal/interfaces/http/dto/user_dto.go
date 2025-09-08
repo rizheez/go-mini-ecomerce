42006cc3eca9e41df90f77d9b5654272b495de77
rizheez <rezamuhammad980@gmail.com>
2025-09-08 21:58:19 +0800

    feat(dto): add user request and response DTOs

--- internal/interfaces/http/dto/user_dto.go
+++ internal/interfaces/http/dto/user_dto.go
@@ -1,1 +1,31 @@
 package dto
+
+type UserRes struct {
+	ID        int    `json:"id"`
+	Email     string `json:"email"`
+	Password  string `json:"-"`
+	Name      string `json:"name"`
+	Phone     string `json:"phone"`
+	Role      string `json:"role"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+type UserReq struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=3,max=32"`
+	Name     string `json:"name" validate:"required,min=3,max=50"`
+	Phone    string `json:"phone" validate:"required,min=10,max=14"`
+	Role     string `json:"role" validate:"required,oneof=admin customer"`
+}
+
+type UserLoginReq struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserLoginRes struct {
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	AccessToken string `json:"access_token"`
+}
